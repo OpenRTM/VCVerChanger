1533ec6f6606c5b32ad1b883821efcd064666b2c
n-kawauchi <n.kawauchi@aist.go.jp>
2019-06-21 16:21:25 +0900

    Changed RTM_VC_VERSION of vc2017 and vc2019 according to OpenRTM 1.2.1 installer.

--- VCVerChanger/VCVerChangerDlg.cpp
+++ VCVerChanger/VCVerChangerDlg.cpp
@@ -248,13 +248,12 @@ void CVCVerChangerDlg::Init()
 
 	VISUAL_STUDIO vc_ver[] = {
 		{"Visual Studio 17", "vc17"},
-		{"Visual Studio 16", "vc16"},
-		{"Visual Studio 15 2017", "vc141"},
+		{"Visual Studio 16 2019", "vc14"},
+		{"Visual Studio 15 2017", "vc14"},
 		{"Visual Studio 14 2015", "vc14"},
 		{"Visual Studio 12 2013", "vc12"},
 		{"Visual Studio 11 2012", "vc11"},
 		{"Visual Studio 10 2010", "vc10"},
-		{"Visual Studio 9 2008", "vc9"},
 	};
 	const int vcNum = sizeof vc_ver / sizeof vc_ver[0];
 
